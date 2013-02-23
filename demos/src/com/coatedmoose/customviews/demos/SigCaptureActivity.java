7cd6d1286e1ca10d43c4ad546cb2b3646e32d2c4
default_git_name <default_git_email>
2013-02-23 17:55:43 +0000

    View contains the custom view.

--- demos/src/com/coatedmoose/customviews/demos/SigCaptureActivity.java
+++ demos/src/com/coatedmoose/customviews/demos/SigCaptureActivity.java
@@ -3,8 +3,6 @@ package com.coatedmoose.customviews.demos;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class SigCaptureActivity extends Activity {
 
@@ -12,13 +10,5 @@ public class SigCaptureActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sig_capture);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_sig_capture, menu);
-        return true;
-    }
-
-    
+    }    
 }
