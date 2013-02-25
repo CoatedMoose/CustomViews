4f412d90e048a7c674bab370e1f4f2d30758bd3f
default_git_name <default_git_email>
2013-02-25 03:19:10 +0000

    Testing a bunch of code.

--- demos/src/com/coatedmoose/customviews/demos/SigCaptureActivity.java
+++ demos/src/com/coatedmoose/customviews/demos/SigCaptureActivity.java
@@ -3,12 +3,21 @@ package com.coatedmoose.customviews.demos;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.util.Log;
+
+import com.coatedmoose.customviews.SignatureView;
 
 public class SigCaptureActivity extends Activity {
 
+	private static final String TAG = "Signature";
     @Override
     public void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sig_capture);
+		SignatureView sigView = (SignatureView) findViewById(R.id.signview);
+		int h = sigView.getHeight();
+		int w = sigView.getWidth();
+		Log.d(TAG, "height: "+h+", width: "+w);
     }    
 }
