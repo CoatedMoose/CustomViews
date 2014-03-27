d93e6499584f3c5bec9491b81c1b2cb422123f26
Andrew Crichton <andrew.g.crichton@gmail.com>
2014-03-27 12:14:40 -0400

    Fix spaces to tabs.

--- library/src/com/coatedmoose/customviews/SignatureView.java
+++ library/src/com/coatedmoose/customviews/SignatureView.java
@@ -79,7 +79,7 @@ public class SignatureView extends View {
 	}
 
 	/**
-     * Clear the signature from the view.
+	 * Clear the signature from the view.
 	 */
 	public void clearSignature() {
 		if (mCanvas != null) {
@@ -90,11 +90,11 @@ public class SignatureView extends View {
 		}
 	}
 
-    /**
-     * Clear the signature from the view.
-     *
-     * @deprecated use {@link clearSignature()} instead (no need to shorten).
-     */
+	/**
+	 * Clear the signature from the view.
+	 *
+	 * @deprecated use {@link clearSignature()} instead (no need to shorten).
+	 */
 	@Deprecated
 	public void clearSig() {
 		clearSignature();
